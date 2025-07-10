c5cdc80a0552c4083c2b8dd7d682ff27b8d74f80
mynul_leo <leo@nogorsolutions.com>
2025-07-10 11:07:06 +0600

    croper service provider

--- src/ImageCropperServiceProvider.php
+++ src/ImageCropperServiceProvider.php
@@ -1,18 +1,13 @@
 <?php
 
-namespace Mynulleo\ImageCropper;
-
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use MynulLeo\ImageCropper\View\Components\ImageCropper;
 
 class ImageCropperServiceProvider extends ServiceProvider
 {
-    public function register()
+    public function boot(): void
     {
-        //
-    }
-
-    public function boot()
-    {
-        //
+        Blade::component('image-cropper', ImageCropper::class);
     }
 }
